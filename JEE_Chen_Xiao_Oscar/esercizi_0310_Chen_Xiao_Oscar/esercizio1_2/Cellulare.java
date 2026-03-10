04e2ae9ebb23b03f20fc4eec8dfa5cd72f44cfd0
Xiao Oscar Chen <x.chn99@gmail.com>
2026-03-10 18:26:36 +0100

    update

--- JEE_Chen_Xiao_Oscar/esercizi_0310_Chen_Xiao_Oscar/esercizio1_2/Cellulare.java
+++ JEE_Chen_Xiao_Oscar/esercizi_0310_Chen_Xiao_Oscar/esercizio1_2/Cellulare.java
@@ -10,19 +10,46 @@ package esercizio1_2;
  * Dotare la classe di quattro metodi: 
  * 
  *     a. Il metodo "chiama", che simula una chiamata, stampando a schermo: "sto
- *     chiamando il numero: ", corredata da un numero di esempio. La chiamata può 
- *     partire solo se il credito è maggiore di 0. 
+ *        chiamando il numero: ", corredata da un numero di esempio. La chiamata può 
+ *        partire solo se il credito è maggiore di 0. 
  *     b. Il metodo "creditoResiduo", che stampa a schermo il credito residuo. 
  *     c. Il metodo "ultimaChiamataEffettuata", che ritorna l'ultimo numero chiamato dall'utente. 
  *     d. Il metodo "richiama", che chiama l'ultimo numero che è stato chiamato. 
  *     e. Il metodo "ricaricaCredito", che ricarica il credito del cellulare. 
  *     
- *     Un consiglio: int potrebbe essere un tipo di dato non adatto 
- *     a contere numeri di telefono, potete usare String.
+ * Un consiglio: int potrebbe essere un tipo di dato non adatto 
+ * a contere numeri di telefono, potete usare String.
  */
 
 public class Cellulare {
 
+	private int credito;
+	private String ultimoNumero;
 	
+	public void creditoResiduo() {
+		System.out.println(credito);
+	}
 	
+	public void chiama() {
+		
+		if(credito>0) {
+			System.out.println("Sto chiamando il numero: 1234567890.");
+			credito--;
+		}
+		else
+			System.out.println("Credito insufficiente.");
+	}
+	
+	public String ultimaChiamataEffettuata() {
+		return ultimoNumero;
+	}
+	
+	public void richiama() {
+		
+	}
+	
+	public void ricaricaCredito() {
+		
+	}
+
 }
--- JEE_Chen_Xiao_Oscar/esercizi_0310_Chen_Xiao_Oscar/esercizio1_2/EsempioCellulare.java
+++ JEE_Chen_Xiao_Oscar/esercizi_0310_Chen_Xiao_Oscar/esercizio1_2/EsempioCellulare.java
@@ -14,5 +14,6 @@ public class EsempioCellulare {
 
 	public static void main(String[] args) {
 		
+		
 	}
 }
