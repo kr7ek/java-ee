9688eb36e28b905c39d5e94ac2a96949e5423389
Xiao Oscar Chen <x.chn99@gmail.com>
2026-03-20 14:10:01 +0100

    update

--- Esercizi/esercizi_0320_Chen_Xiao_Oscar/esercizio7/Main.java
+++ Esercizi/esercizi_0320_Chen_Xiao_Oscar/esercizio7/Main.java
@@ -5,10 +5,15 @@ public class Main {
 	public static void main(String[] args) throws UndiscoveredDimensionException {
 		// TODO Auto-generated method stub
 
+		System.out.println("Creazione di una scatola di dimensioni 10x20x30:");
 		Box box1 = new Box(10, 20, 30);
+		System.out.println("Creazione di una scatola di dimensioni 15x25x35:");
 		Box box2 = new Box(15, 25, 35);
+		System.out.println("Creazione di una scatola di dimensioni 10x-3x30:");
+		Box box3 = new Box(10, -3, 30); // Questo solleverà un'eccezione
 
 		// Verifica se le due scatole sono uguali
+		System.out.println("Verifica se le scatole 1 e 2 sono uguali:");
 		if (box1.getAltezza() == box2.getAltezza() && box1.getLunghezza() == box2.getLunghezza()
 				&& box1.getProfondita() == box2.getProfondita()) {
 			System.out.println("Le due scatole sono uguali.");
