db24795e8aec8527729f05ed6741261611cd051a
Xiao Oscar Chen <x.chn99@gmail.com>
2026-03-20 12:44:32 +0100

    update

---
+++ Esercizi/esercizi_0320_Chen_Xiao_Oscar/esercizio8_9_10/Main.java
@@ -0,0 +1,30 @@
+package esercizio8_9_10;
+
+/*
+ * Creare un progetto in Java per un contesto di gestione dell’inventario di un negozio di elettronica. 
+ * In tale progetto dovranno esserci le classi Dispositivo, Computer e Smartphone.
+ * In particolare quest’ultime due estendono la classe Dispositivo, 
+ * ed in particolare Computer implementerà anche il metodo inserisciPeriferica(), mentre Smartphone il metodo mandaMessaggio().
+ * Ogni Dispositivo dovrà avere un attributo sistemaOperativo 
+ * che utilizza un’enumerazione per specificare il sistema operativo utilizzato (WINDOWS, MACOS, LINUX, ANDROID E IOS). 
+ * Creare una classe GestioneInventario che gestisce un array di dispositivi, 
+ * in particolare bisogna poter aggiungere i dispositivi, 
+ * rimuovere i dispositivi e stampare i dettagli a video di tutti i dispositivi presenti nell’inventario.
+ * Creare nel metodo main() degli oggetti di tipo Computer e Smartphone applicando il polimorfismo per dati. Fornire dei casi di test.
+ * 
+ * Creare un metodo statico al di fuori del metodo main() che richiede in ingresso un parametro di tipo GestioneInventario. 
+ * Tale metodo dovrà calcolare quanti dispositivi per ogni tipologia di sistema operativo contiene l’inventario e stampare il resoconto a video.
+ * 
+ * Creare un metodo statico al di fuori del metodo main() che richiede in ingresso un parametro polimorfo di tipo Dispositivo, 
+ * se tale oggetto è un Computer invocare il metodo inserisciPeriferica(), 
+ * se invece si tratta di uno Smartphone, invocare il metodo mandaMessaggio().
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
